5538a6a2259b954af5e230d9ea5dfec78cdc26c9
Lewis <lewisnpakoti@gmail.com>
2021-03-02 17:01:58 +1300

    added all of basic html plus links to style and script files

--- index.js
+++ index.js
@@ -3,5 +3,14 @@
 const express = require('express')
 
 const server = express()
+const port = 3000
 
-server.listen(3000)
+server.get('/', (req, res) => {
+    res.send('Cool, eh?!')
+})
+
+server.listen(port, () => {
+    console.log('Server is listening to port', port)
+})
+
+// 2. Set route
