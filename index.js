be726d7073f16c7aaefe5ba6b868c6c40931c507
Lewis <lewisnpakoti@gmail.com>
2021-03-04 17:11:46 +1300

    added /test-html route with html test file. It works

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 // 1. Require Express
 
+const path = require('path')
 const express = require('express')
 
 const server = express()
 const port = 3000
 
-server.get('/', (req, res) => {
-    res.send('Cool, eh?!')
-})
-
 server.listen(port, () => {
     console.log('Server is listening to port', port)
 })
 
-// 2. Set route
+// 2. Set routes
+
+server.get('/', (req, res) => {
+    res.send('This working?!')
+})
+
+server.get('/test-html', (req, res) => {
+    const filename = path.join(__dirname, 'test.html')
+    res.sendFile(filename)
+})
